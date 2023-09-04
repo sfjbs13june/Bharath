2ba4e2e0c5a5d5aba8628b855be68529318557bd
admin <Bharath@gmail.com>
2023-09-03 23:18:25 -0700

    First web service

---
+++ day02/first-microservice/Bharath-web-microservice/src/main/java/com/Bharath/app/BharathWebMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.Bharath.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharathWebMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharathWebMicroserviceApplication.class, args);
+	}
+
+}
---
+++ day02/first-microservice/Bharath-web-microservice/src/test/java/com/Bharath/app/BharathWebMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bharath.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BharathWebMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
