5f4a8f61b8123154010c54f4b33d83f692dec5b9
admin <Bharath@gmail.com>
2023-09-06 23:39:12 -0700

    student

---
+++ day06/spring-student-app/src/main/java/com/student/app/springstudentapp/SpringStudentAppApplication.java
@@ -0,0 +1,13 @@
+package com.student.app.springstudentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudentAppApplication.class, args);
+	}
+
+}
---
+++ day06/spring-student-app/src/test/java/com/student/app/springstudentapp/SpringStudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.app.springstudentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
