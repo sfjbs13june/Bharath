bf4482a9b096638071a907168a7a88156287010b
admin <Bharath@gmail.com>
2023-09-13 00:25:11 -0700

    mongo db

---
+++ day09/spring-mysql-app1/src/main/java/com/bharath/app/repository/HospitalDataRepository.java
@@ -0,0 +1,11 @@
+//package com.bharath.app.repository;
+//
+//import com.bharath.app.model.hospital;
+//import com.bharath.app.model.patient;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import java.util.UUID;
+//
+//public interface HospitalDataRepository extends CrudRepository<Hospital, Integer> {
+//}
+//
---
+++ day09/spring-mysql-app1/src/main/java/com/bharath/app/repository/PatientRepository.java
@@ -0,0 +1,6 @@
+package com.bharath.app.repository;
+
+import com.bharath.app.model.Patient;
+import org.springframework.data.repository.CrudRepository;
+public interface PatientRepository extends CrudRepository<Patient, Integer>{
+}
